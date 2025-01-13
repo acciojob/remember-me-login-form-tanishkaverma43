2c4c4470108be6ff158e808842b7f93c0ca4c865
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-13 13:40:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var remember = document.getElementById('remember').checked;
+    
+    if (remember) {
+        localStorage.setItem('username', username);
+        localStorage.setItem('password', password);
+    } else {
+        localStorage.removeItem('username');
+        localStorage.removeItem('password');
+    }
+    
+    alert('Logged in as ' + username);
+});
