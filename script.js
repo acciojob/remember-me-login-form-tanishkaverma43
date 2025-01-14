ce4b3b9fbad5c3127145e32c1c6f042a26456e2f
Tanishka Verma <152250719+tanishkaverma43@users.noreply.github.com>
2025-01-14 10:26:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ document.getElementById('submit').addEventListener('click', function(event) {
     if (remember) {
         localStorage.setItem('username', username);
         localStorage.setItem('password', password);
+        var btn = document.createElement("BUTTON");
+        btn.innerHTML = "Login as existing user";
+        btn.id = "existing";
+        document.getElementById("buttons").appendChild(btn);
     } else {
         localStorage.removeItem('username');
         localStorage.removeItem('password');
